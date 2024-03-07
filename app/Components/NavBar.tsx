935ed05260abb501e42b2dbe9b1e4a4b0b6bab4c
Hamza Lomari <lomari.hamza.it@gmail.com>
2024-03-07 18:33:30 +0100

    :rocket: Finished project

---
+++ app/Components/NavBar.tsx
@@ -0,0 +1,12 @@
+import Link from "next/link";
+import { ModeToggle } from "./ModeToggle";
+export default function NavBar() {
+  return (
+    <nav className="w-full relative flex items-center justify-between max-w-2xl mx-auto px-4 py-5">
+      <Link href="/" className="font-bold text-3xl">
+        Hamza<span className="text-gray-500">Blog</span>
+      </Link>
+      <ModeToggle />
+    </nav>
+  );
+}
---
+++ app/blog/[slug]/page.tsx
@@ -0,0 +1,46 @@
+import { fullBlog } from "@/app/lib/interface";
+import { client, urlFor } from "@/app/lib/sanity";
+import { PortableText } from "@portabletext/react";
+import Image from "next/image";
+
+async function getData(slug: string) {
+  const query = `*[_type == 'blog' && slug.current == '${slug}']{
+      "currentSlug": slug.current,
+        title,
+        content,
+        titleImage
+    }[0]`;
+  const data = client.fetch(query);
+  return data;
+}
+
+export default async function BlogArticle({
+  params,
+}: {
+  params: { slug: string };
+}) {
+  const data: fullBlog = await getData(params.slug);
+  return (
+    <div className="mt-8">
+      <h1>
+        <span className="block text-base text-center text-primary tracking-wide font-semibold">
+          Hamza Lomari - Blog
+        </span>
+        <span className="mt-2 block text-3xl text-center leading-8 font-bold tracking-tight sm:text-4xl">
+          {data.title}
+        </span>
+      </h1>
+      <Image
+        src={urlFor(data.titleImage).url()}
+        alt="image"
+        height={800}
+        width={800}
+        priority
+        className="mt-8 inset-0 rounded-lg border-black border-2 dark:border-none"
+      />
+      <div className="mt-16 prose prose-xl prose-li:marker:text-secondary dark:prose-invert">
+        <PortableText value={data.content}/>
+      </div>
+    </div>
+  );
+}
